6fa93e6f634614ff4bd1394e41510c9c0e2c8298
TyphousCrane654 <simsim.bartos@gmail.com>
2021-08-14 13:02:39 +0200

    Background update
    -background will now move using gyroscope on mobile devices

--- assets/JavaScript/background.js
+++ assets/JavaScript/background.js
@@ -2,7 +2,7 @@ VANTA.HALO({
   el: "#animated-background",
   mouseControls: true,
   touchControls: true,
-  gyroControls: false,
+  gyroControls: true,
   minHeight: 200.00,
   minWidth: 200.00,
   xOffset: -0.21
